73658eff15b3b30d2439cf40004823a61ea15fc5
tbhazari <tbhazari@gmail.com>
2019-11-05 11:06:45 +0200

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,7 +26,7 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
+    //Task 2 - Launch the Google Maps Activity
     // ***
     public void createMapIntent(View view){
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
@@ -37,6 +37,13 @@ public class InfoActivity extends AppCompatActivity {
 
 
     // ***
-    // TODO - Task 3 - Launch the Phone Activity
+    // Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:0123456789"));
+        startActivity(intent);
+    }
 }
